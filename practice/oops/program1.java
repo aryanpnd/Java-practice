013faad4c0fbe9a3b04d7cbbf0c86ede7a776873
aryanpnd <captainaryan9870@gmail.com>
2023-08-19 00:42:40 +0530

    Oops

--- practice/oops/program1.java
+++ practice/oops/program1.java
@@ -1,5 +1,41 @@
+class Car{
+    String name;
+    String color;
+    String fuelType;
+
+    public void start(){
+        System.out.println("started");
+    }
+
+    public void printDetails(){
+        System.out.println("Car name : "+this.name);
+        System.out.println("Car color : "+this.color);
+        System.out.println("Car fuel type : "+this.fuelType);
+    }
+}
+
+class Customer{
+    String name;
+    int age;
+
+    void printCustomerDetails(){
+        System.out.println("Customer name : "+this.name);
+        System.out.println("Customer age : "+this.age);
+    }
+}
+
 public class program1 {
     public static void main(String[] args) {
-        
+        Car car = new Car();
+        car.color="black";
+        car.name = "alto";
+        car.fuelType = "petrol";
+        car.start();
+        car.printDetails();
+
+        Customer cust = new Customer();
+        cust.name = "Aryan";        
+        cust.age = 18;        
+        cust.printCustomerDetails();
     }
 }
