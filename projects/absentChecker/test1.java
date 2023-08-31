7bcdc38131b6653789a25304e53b8404d26b2d05
aryanpnd <captainaryan9870@gmail.com>
2023-08-31 08:17:22 +0530

    absent checker

--- projects/absentChecker/test1.java
+++ projects/absentChecker/test1.java
@@ -49,7 +49,7 @@ public class test1 {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number of students : ");
+        System.out.println("enter the number of students: ");
         String rollNo = sc.nextLine();
         String[] noOfStudent = rollNo.split(",");
 
